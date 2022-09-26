14f1aeea886c06e65d6326f2672583e8025ec264
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-09-26 05:19:02 +0200

    add header | install framer motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,44 @@
+import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
+
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-10 xl:items-center">
+      <motion.section
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://github.com/alexander-rusiecki"
+          target="_blank"
+          fgColor="black"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/alexander-rusiecki-b1428519b/"
+          target="_blank"
+          fgColor="black"
+          bgColor="transparent"
+        />
+      </motion.section>
+      <motion.section
+        initial={{ x: 500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="black"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm">Contact me</p>
+      </motion.section>
     </header>
   );
 };
