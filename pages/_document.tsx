0f0748132f2e70e0be3b0799da1118c03587af66
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-10-01 14:30:30 +0200

    add Google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 const Document = () => {
   return (
     <Html lang="en" dir="ltr">
       <Head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        ></Script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
         <meta charSet="utf-8" />
         <meta
           name="description"
