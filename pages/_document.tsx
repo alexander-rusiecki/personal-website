f4516e148602d765556deaaa0bb50bd45b3f2e08
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-10-03 11:33:23 +0200

    fix Google analytics tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ const Document = () => {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
           `}
         </Script>
         <meta charSet="utf-8" />
